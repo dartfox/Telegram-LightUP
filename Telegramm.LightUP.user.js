241c87ca8559506bc9cdc5dc0d240864036266af
dartfox <lazarrus@yandex.ru>
2016-03-01 20:28:14 +0300

    Update Telegramm.LightUP.user.js

--- Telegramm.LightUP.user.js
+++ Telegramm.LightUP.user.js
@@ -11,14 +11,25 @@
 'use strict';
 
 // Your code here...
-var author_list_khaki=[
+var author_list_Khaki=[
         'Nick1','Nick2'
 ];
+var author_list_DarkSeaGreen=[
+        'Nick3','Nick4'
+];
+
 setInterval(function(){
     $.each(author_list_khaki,function(i,v){
       jQuery(".im_history_messages_peer .im_message_author:contains('"+v+"')").closest('.im_message_outer_wrap').each(function(){
         if(!$(this).is('.khaki')) {
            $(this).addClass('khaki').css('background-color','khaki')
+        }
+      });
+    });
+    $.each(author_list_DarkSeaGreen,function(i,v){
+      jQuery(".im_history_messages_peer .im_message_author:contains('"+v+"')").closest('.im_message_outer_wrap').each(function(){
+        if(!$(this).is('.DarkSeaGreen')) {
+           $(this).addClass('DarkSeaGreen').css('background-color','DarkSeaGreen')
         }
       });
     });
