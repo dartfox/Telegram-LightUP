090f41f1492840c3c1508f96e0f5193baf0b299e
dartfox <lazarrus@yandex.ru>
2016-03-01 20:13:56 +0300

    Update Telegramm.LightUP.user.js

--- Telegramm.LightUP.user.js
+++ Telegramm.LightUP.user.js
@@ -11,14 +11,14 @@
 'use strict';
 
 // Your code here...
-var author_list=[
+var author_list_khaki=[
         'MakcEXPLO Е14 Мsk [RU]'
 ];
 setInterval(function(){
-    $.each(author_list,function(i,v){
+    $.each(author_list_khaki,function(i,v){
       jQuery(".im_history_messages_peer .im_message_author:contains('"+v+"')").closest('.im_message_outer_wrap').each(function(){
-        if(!$(this).is('.yellow')) {
-           $(this).addClass('yellow').css('background-color','yellow')
+        if(!$(this).is('.khaki')) {
+           $(this).addClass('khaki').css('background-color','khaki')
         }
       });
     });
